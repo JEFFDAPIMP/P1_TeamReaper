66dcc2c502c3b42bfda63d43bb3ed2785452eaf9
JEFFDAPIMP <jeffrey.baltierrez@gmail.com>
2025-02-12 15:00:02 -0600

    Fix healing, now works as expected, no collision required.

---
+++ Assets/Scripts/HealParent.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class HealParent : MonoBehaviour
+{
+    [SerializeField] private int healAmount = 1;
+
+    // When spawned, get parent health componment, "damage" it for negative heal amount, then delete this game object.
+    void Start()
+    {
+        this.gameObject.GetComponentInParent<Health>().Damage((healAmount * -1), Health.allDamageType.Crunch);
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/PlayerCombat/PlayerAttack.cs
+++ Assets/Scripts/PlayerCombat/PlayerAttack.cs
@@ -111,11 +111,22 @@ public class PlayerAttack : MonoBehaviour
     /// <param name="fireInput">player input fire mode</param>
     private void shootBullet(PlayerWeapon weapon, int fireInput)
     {
-        GameObject bullet = Instantiate(((fireInput == 1) ? weapon.bulletPrefab1 : weapon.bulletPrefab2), bulletSpawnTransform);
-        bullet.layer = LayerMask.NameToLayer(playerProjectileLayerMaskName);
-        bullet.transform.SetParent(null);
-        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * weapon.bulletSpeed1;
-        weapon.currentAmmoCount--;
+        if(weapon.bulletPrefab1.name == "Pez")
+        {
+            //For Pez we set this gameobject as its parent, so the heal script can quickly add health to the originator of the bullet
+            GameObject bullet = Instantiate(((fireInput == 1) ? weapon.bulletPrefab1 : weapon.bulletPrefab2), bulletSpawnTransform);
+            bullet.transform.SetParent(this.gameObject.transform);
+            weapon.currentAmmoCount--;
+        }
+        else
+        {
+            GameObject bullet = Instantiate(((fireInput == 1) ? weapon.bulletPrefab1 : weapon.bulletPrefab2), bulletSpawnTransform);
+            bullet.layer = LayerMask.NameToLayer(playerProjectileLayerMaskName);
+            bullet.transform.SetParent(null);
+            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * weapon.bulletSpeed1;
+            weapon.currentAmmoCount--;
+        }
+
     }
 
     /// <summary>
